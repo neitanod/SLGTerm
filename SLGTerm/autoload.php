7c2d0e1896528fff47f3f51b3ecb3a7cd2238138
Sebastián Grignoli <grignoli@gmail.com>
2021-01-01 20:19:19 -0300

    Fake autoload

---
+++ SLGTerm/autoload.php
@@ -0,0 +1,10 @@
+<?php
+// fake autoloader that just loads everything.
+// TODO: a real autoloader that loads classes on demand.
+
+require(__DIR__."/SLGTerm.php");
+require(__DIR__."/Terminal.php");
+require(__DIR__."/Cursor.php");
+require(__DIR__."/Label.php");
+require(__DIR__."/Spinner.php");
+require(__DIR__."/Input.php");
